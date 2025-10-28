a988f8d6b42c32e99ad7443a26c8c221895217e3
Muhammad Arrahman <arrahmanmuhammad16@gmail.com>
2025-10-29 00:49:46 +0800

    update register screen

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  final TextEditingController confirmController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+
+  }
+}
